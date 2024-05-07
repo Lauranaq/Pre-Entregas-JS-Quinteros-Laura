9d7449197663fb16281bcb14c1013d58db82e872
Laura Quinteros <lauranaq3@hotmail.com>
2024-05-06 22:19:05 -0300

    Proyecto Final Correcciones

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 const carrito = [];
-//Arrays con productos=prestamos
+//Array con productos=prestamos
 const productos = [
     {
         nombre: "Prestamo para compra o reparacion de Vehiculos",
@@ -40,118 +40,28 @@ const productos = [
     },
              
 ];
-//Arrays de cuotas 
+//Array de cuotas 
 const cuotas = [1, 2,3,4,5,6];
 
+
+// Guardo mi Objeto Prestamo en el Local Storage
+let productosJSON = JSON.stringify(productos);
+localStorage.setItem( "productos", productosJSON);
+
+console.log(productosJSON);
+
+// Guardo mi Objeto Cuotas en el Local Storage
+let cuotasJSON = JSON.stringify (cuotas);
+localStorage.setItem ("cuotas", cuotasJSON);
+
+console.log(cuotasJSON);
+
+
+//Creo constantes para utilizar 
 const contenedorProductos = document.querySelector("#productos");
 const carritoVacio = document.querySelector("#carrito-vacio");
 const carritoProductos = document.querySelector("#carrito-productos");
 const carritoTotal = document.querySelector("#carrito-total");
-
-
-// Recorro Array de Cuotas 
-const container = document.getElementById('containerCuotas');
-const selectElement = document.createElement('select');
-selectElement.setAttribute('id', 'selectCuotas');
-cuotas.forEach(cuota => {
-    const option = document.createElement('option'); 
-    option.value = cuota 
-    option.text = cuota;  
-    selectElement.appendChild(option);
-});
-container.appendChild(selectElement);
-
-//Funcion para calcular el valor de la cuota 
-function calcularCuota(montoTotal, cuotaElegida) {
-    let interesInicial = 0.05; // 5%
-    let incrementoInteres = 0.02; // 2%
-    let interesTotal = interesInicial + (cuotaElegida - 1) * incrementoInteres;
-    let cuotaConInteres = montoTotal * (1 + interesTotal) / cuotaElegida;
-    return cuotaConInteres.toFixed(2);
-}
-
-// Función para mostrar las cuotas
-const mostrarCuotas = () => {
-    const cuotaSeleccionada = parseInt(document.getElementById('selectCuotas').value);
-    const montoTotal = parseInt(carritoTotal.innerText.slice(1));
-    const cuotaCalculada = calcularCuota(montoTotal, cuotaSeleccionada);
-    document.getElementById('cuotaSeleccionada').innerText = `El valor de cada cuota seria: $${cuotaCalculada}`;
-};
-
-// Evento para detectar cambios en la selección de cuotas
-document.getElementById('selectCuotas').addEventListener('change', mostrarCuotas);
-
-
-//Funcion para actualizar el carrito con prestamos seleccionados
-function actualizarCarrito() {
-    if (carrito.length === 0) {
-        carritoVacio.classList.remove("d-none");
-        carritoProductos.classList.add("d-none");
-    } else {
-        carritoVacio.classList.add("d-none");
-        carritoProductos.classList.remove("d-none");
-
-        carritoProductos.innerHTML = "";
-        carrito.forEach(producto => {
-            const div = document.createElement("div");
-            div.classList.add("carrito-producto");
-            div.innerHTML = `
-                <h3>${producto.nombre}</h3>                
-                <p>Subt: $${producto.monto}</p>
-            `;
-
-            const btn = document.createElement("button");
-            btn.classList.add("carrito-producto-btn");
-            btn.innerText = "✖️";
-            btn.addEventListener("click", () => {
-                borrarDelCarrito(producto);
-            })
-            div.append(btn);
-
-            carritoProductos.append(div);
-        })
-    }
-    actualizarTotal();
-}
-
-
-//Funcion para agregar el prestamo seleccionado
-const agregarAlCarrito = (producto) => {
-    if (producto.cantidad > 0){ 
-        const itemEncontrado = carrito.find(item => item.nombre === producto.nombre);
-        if (itemEncontrado) {
-            itemEncontrado.cantidad++;
-            producto.cantidad--;
-        } else {
-            carrito.push({...producto, cantidad: 1});
-            producto.cantidad--;
-        }
-    }else {
-        Swal.fire({
-            title: "Lo sentimos!",
-            text: "El prestamo solo puede elegirse una vez",
-            icon: "error"
-          });
-
-             
-    } 
-        actualizarCarrito();
-        mostrarCuotas();
-}
-
-//Funcion para borrar algun prestamo seleccionado
-const borrarDelCarrito = (producto) => {
-    const prodIndex = carrito.findIndex(item => item.nombre === producto.nombre);
-    carrito.splice(prodIndex, 1);
-    actualizarCarrito();
-    mostrarCuotas();
-}
-
-//Funcion para actualizar el monto total del prestamo
-const actualizarTotal = () => {
-    const total = carrito.reduce((acc, prod) => acc + prod.monto , 0);    
-    carritoTotal.innerText = `$${total}`;
-}
 
 
 // Recorro el array de prestamos 
@@ -176,6 +86,153 @@ productos.forEach((producto) => {
     contenedorProductos.append(div);
 })
 
+
+//Funcion para actualizar el carrito con prestamos seleccionados
+function actualizarCarrito() {
+    if (carrito.length === 0) {
+        carritoVacio.classList.remove("d-none");
+        carritoProductos.classList.add("d-none");
+    } else {
+        carritoVacio.classList.add("d-none");
+        carritoProductos.classList.remove("d-none");
+
+        carritoProductos.innerHTML = "";
+        carrito.forEach(producto => {
+            const div = document.createElement("div");
+            div.classList.add("carrito-producto");
+            div.innerHTML = `
+                <h3>${producto.nombre}</h3>                
+                <p> $${producto.monto}</p>
+            `;
+
+            const btn = document.createElement("button");
+            btn.classList.add("carrito-producto-btn");
+            btn.innerText = "✖️";
+            btn.addEventListener("click", () => {
+                borrarDelCarrito(producto);
+            })
+            div.append(btn);
+
+            carritoProductos.append(div);
+        })
+    }
+    actualizarTotal();
+}
+
+// Recorro Array de Cuotas 
+const container = document.getElementById('containerCuotas');
+const selectElement = document.createElement('select');
+selectElement.setAttribute('id', 'selectCuotas');
+
+selectElement.classList.add('miEstiloSelect');
+
+cuotas.forEach(cuota => {
+    const option = document.createElement('option'); 
+    option.value = cuota 
+    option.text = cuota;  
+    selectElement.appendChild(option);
+});
+container.appendChild(selectElement);
+
+//Funcion para calcular el valor de la cuota 
+function calcularCuota(montoTotal, cuotaElegida) {
+    let interesInicial = 0.05; 
+    let incrementoInteres = 0.02; 
+    let interesTotal = interesInicial + (cuotaElegida - 1) * incrementoInteres;
+    let cuotaConInteres = montoTotal * (1 + interesTotal) / cuotaElegida;
+    return cuotaConInteres.toFixed(2);
+}
+
+// Función para mostrar las cuotas
+const mostrarCuotas = () => {
+    const cuotaSeleccionada = parseInt(document.getElementById('selectCuotas').value);
+    const montoTotal = parseInt(carritoTotal.innerText.slice(1));
+    const cuotaCalculada = calcularCuota(montoTotal, cuotaSeleccionada);
+    document.getElementById('cuotaSeleccionada').innerText = `El valor de cada cuota seria: $${cuotaCalculada}`;
+};
+
+// Evento para detectar cambios en la selección de cuotas
+document.getElementById('selectCuotas').addEventListener('change', mostrarCuotas);
+
+
+
+//Funcion para agregar el prestamo seleccionado
+const agregarAlCarrito = (producto) => {
+    if (producto.cantidad > 0){ 
+        const itemEncontrado = carrito.find(item => item.nombre === producto.nombre);
+        if (itemEncontrado) {
+            itemEncontrado.cantidad++;
+            producto.cantidad--;
+        } else {
+            carrito.push({...producto, cantidad: 1});
+            producto.cantidad--;
+        }
+    }else {
+        Swal.fire({
+            title: "Lo sentimos!",
+            text: "El prestamo solo puede elegirse una vez",
+            icon: "error"
+          });
+
+             
+    } 
+        actualizarCarrito();
+        mostrarCuotas();
+        
+}
+
+//Funcion para borrar algun prestamo seleccionado
+const borrarDelCarrito = (producto) => {
+    const prodIndex = carrito.findIndex(item => item.nombre === producto.nombre);
+    carrito.splice(prodIndex, 1);
+    actualizarCarrito();
+    mostrarCuotas();
+}
+
+//Funcion para actualizar el monto total del prestamo
+const actualizarTotal = () => {
+    const total = carrito.reduce((acc, prod) => acc + prod.monto , 0);    
+    carritoTotal.innerText = `$${total}`;
+}
+
+
+//FORMULARIO DE CONTACTO 
+// Verifico que el usuario sea mayor de 21 años
+const botonEnviar = document.getElementById("botonEnviar");
+ 
+botonEnviar.addEventListener("click", function(event) {       
+    event.preventDefault();       
+    verificar();
+});
+
+
+
+
+const verificar = ()=>{
+    const edad = parseInt(document.getElementById("edad").value);
+    if (edad < 21) {
+        Swal.fire({
+            title: "Lo sentimos!",
+            text: "Por tu edad, no podemos ofrecerte nuestros servicios",
+            icon: "error"
+          });          
+    }else if ((edad === "" || isNaN(edad))){
+        Swal.fire({
+            title: "Lo sentimos!",
+            text: "Se debe completar el campo con tu edad",
+            icon: "error"
+          });
+          document.getElementById("edad").value = "";
+        }else {
+            Swal.fire({
+                title: "Felicitaciones! Ya estas mas cerca de obtener tu prestamo",
+                text: "Completa tu mail en el siguiente casillero y un equipo de asesores se comunicara con vos",
+                icon: "success"
+              }); 
+            }
+   
+}; 
+  
 
 
 
